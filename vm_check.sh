ab56574be03068903d5df750b31915715193d517
rongdede <admin@91yun.org>
2016-03-21 15:04:42 +0800

    添加了yum安装编译器

--- vm_check.sh
+++ vm_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
-wget -q http://people.redhat.com/~rjones/virt-what/files/virt-what-1.12.tar.gz
-tar zxvf virt-what-1.12.tar.gz 1>/dev/null 2>&1
+yum install -y gcc gcc-c++ gdb
+wget http://people.redhat.com/~rjones/virt-what/files/virt-what-1.12.tar.gz
+tar zxvf virt-what-1.12.tar.gz
 cd virt-what-1.12/
-./configure 1>/dev/null 2>&1
-make 1>/dev/null 2>&1
-make install 1>/dev/null 2>&1
+./configure
+make && make install
 virt-what
