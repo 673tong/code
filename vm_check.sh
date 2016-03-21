7f957199ca567fddbbc5aa29980c8c074655b454
rongdede <admin@91yun.org>
2016-03-21 14:33:36 +0800

    不显示过程内容
    不显示过程内容

--- vm_check.sh
+++ vm_check.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
-wget http://people.redhat.com/~rjones/virt-what/files/virt-what-1.12.tar.gz 1>/dev/null 2>&1
+wget -q http://people.redhat.com/~rjones/virt-what/files/virt-what-1.12.tar.gz
 tar zxvf virt-what-1.12.tar.gz 1>/dev/null 2>&1
 cd virt-what-1.12/
 ./configure 1>/dev/null 2>&1
-make && make install 1>/dev/null 2>&1
+make 1>/dev/null 2>&1
+make install 1>/dev/null 2>&1
 virt-what
